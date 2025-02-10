6102ef71cf1c34566453be5ed0e5eb84cc219e63
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-02-10 12:48:37 +0100

    <fix> update footer and add dashboard.js - css and html

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const SMALL_SCREEN_MENU = document.getElementById("mobile-menu");
+  const menuSmallScreen = document.getElementById("mobile-menu");
 
   // Redirigir al link seleccionado
-  SMALL_SCREEN_MENU.addEventListener("change", function () {
-    const SELECTED_VALUE = this.value;
-    if (SELECTED_VALUE) {
-      window.location.href = SELECTED_VALUE;
+  menuSmallScreen.addEventListener("change", function () {
+    const selectedValue = this.value;
+    if (selectedValue) {
+      window.location.href = selectedValue;
     }
   });
 });
