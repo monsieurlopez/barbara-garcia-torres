1a4e47d7b162c9382d123d12012af4daed90e272
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-02-16 07:57:42 +0100

    <fix> add file app.js to controle de js global

---
+++ js/app.js
@@ -0,0 +1,13 @@
+//* Funciones globales **/
+document.addEventListener("DOMContentLoaded", function () {
+  const menuSmallScreen = document.getElementById("mobile-menu");
+
+  // Redirigir al link seleccionado
+  menuSmallScreen.addEventListener("change", function () {
+    const selectedValue = this.value;
+    if (selectedValue) {
+      window.location.href = selectedValue;
+    }
+  });
+});
+
