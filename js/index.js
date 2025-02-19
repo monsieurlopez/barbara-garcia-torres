15a0bc4b6333c263e461b3b8b96c2619b642f70b
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-02-19 08:32:07 +0100

    <fix> change style footer-card services svg for phrase

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-const svgLink = `<?xml version="1.0" ?><svg class="feather feather-external-link" fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/><polyline points="15 3 21 3 21 9"/><line x1="10" x2="21" y1="14" y2="3"/></svg>`;
-
 document.addEventListener("DOMContentLoaded", () => {
   const services = [
     {
@@ -58,7 +56,7 @@ document.addEventListener("DOMContentLoaded", () => {
           .join("")}
       </ul>
       <div class="card-footer card__footer">
-        <a href="${service.url}" class="card-link">${svgLink}</a>
+        <a href="${service.url}" class="card__footer-link"> Ver más </a>
       </div>
     </div>
   `
