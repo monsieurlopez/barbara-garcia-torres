3a54c8f96922e41e6addb5559c05accf7b884d3d
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-02-25 18:54:22 +0100

    <feat> update cards services

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const services = [
     {
-      name: "Mercados",
-      img: "./assets/images/img-mercados.png",
+      name: "Ateliers Allaitement",
+      img: "./assets/images/img-allaitement.png",
       description:
         "Seguimiento continuo de las principales criptomonedas y divisas.",
       ventajas: [
@@ -10,11 +10,11 @@ document.addEventListener("DOMContentLoaded", () => {
         "Cotizaciones en tiempo real",
         "Tablas personalizadas",
       ],
-      url: "pages/mercados.html",
+      url: "",
     },
     {
-      name: "Conversor",
-      img: "./assets/images/img-conversor.png",
+      name: "Kinésithérapie",
+      img: "./assets/images/img-kine.png",
       description:
         "Convierte entre diferentes divisas con tasas de cambio actualizadas.",
       ventajas: [
@@ -22,18 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
         "Fácil de usar",
         "30 monedas disponibles",
       ],
-      url: "pages/conversor.html",
-    },
-    {
-      name: "Noticias",
-      img: "./assets/images/img-noticias.png",
-      description: "Mantente informado con las últimas noticias del mundo.",
-      ventajas: [
-        "Noticias actualizadas",
-        "Buscador por palabra clave",
-        "Link a la noticia",
-      ],
-      url: "pages/noticias.html",
+      url: "",
     },
   ];
 
@@ -44,7 +33,7 @@ document.addEventListener("DOMContentLoaded", () => {
       (service) => `
     <div class="card" style="width: 18rem;">
       <div class="card__header">
-        <img src="${service.img}" class="img-fluid" alt="${service.name}">
+        <img src="${service.img}" class="card-img-top" alt="${service.name}">
       </div>
       <div class="card-body card__body">
         <h5 class="card-title"><strong>${service.name}</strong></h5>
