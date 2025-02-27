a562ab9c3e184380d7157dc1d5d5ad86e4b3c3d2
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-02-27 07:18:32 +0100

    <fix> modif cards services. Delete list items and add description

--- js/index.js
+++ js/index.js
@@ -3,25 +3,13 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       name: "Consultante en allaitement",
       img: "./assets/images/img-allaitement.png",
-      description:
-        "",
-      ventajas: [
-        "Conseils d'autres mamans et papas",
-        "Gratuits",
-        "Groupes réduits",
-      ],
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       url: "pages/lactancia.html",
     },
     {
       name: "Soin en kinésithérapie",
       img: "./assets/images/img-kine.png",
-      description:
-        "",
-      ventajas: [
-        "Nouveaux-nés et enfants",
-        "Adultes",
-        "Gynécologiques ",
-      ],
+      description: "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?",
       url: "pages/soins.html",
     },
   ];
@@ -38,11 +26,6 @@ document.addEventListener("DOMContentLoaded", () => {
       <div class="card-body card__body">
         <h5 class="card-title"><strong>${service.name}</strong></h5>
         <p class="card-text">${service.description}</p>
-        <ul class="list-group list-group-flush">
-        ${service.ventajas
-          .map((ventaja) => `<li class="list-group-item">${ventaja}</li>`)
-          .join("")}
-      </ul>
       </div>
       <div class="card-footer card__footer">
         <a href="${service.url}" class="card__footer-link"> Voir plus </a>
