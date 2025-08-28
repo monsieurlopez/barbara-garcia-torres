5833ee1c1e40b3b8fb90783ac3bd00979099c2c5
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-08-29 00:58:35 +0200

    <fix> correction show header and footer contacto.html

--- src/main.js
+++ src/main.js
@@ -1,7 +1,1 @@
-import "./contacto.js";
-
-window.__ENV__ = {
-  VITE_EMAILJS_PUBLIC_KEY: import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
-  VITE_EMAILJS_SERVICE_ID: import.meta.env.VITE_EMAILJS_SERVICE_ID,
-  VITE_EMAILJS_TEMPLATE_ID: import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-};
+import "./css/style.css";
