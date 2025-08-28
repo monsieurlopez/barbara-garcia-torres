1a45c2a1f727115789fcdf6355852c99364d4281
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-08-28 18:40:03 +0200

    <feat> update main.js with all css styles

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import "./css/style.css";
 import "./css/index.css";
+import "./css/lactancia.css";
+import "./css/soins.css";
+import "./css/aviso-legal.css";
+import "./css/contacto.css";
 
 import "./js/app.js";
 import "./js/index.js";
