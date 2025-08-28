7d1f1d69de448da82cb69677836442856f777a3e
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-08-28 19:51:46 +0200

    <fix> add type='module' to scripts

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import path from "path";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "src/index.html"),
+        lactancia: path.resolve(__dirname, "src/pages/lactancia.html"),
+        soins: path.resolve(__dirname, "src/pages/soins.html"),
+        contacto: path.resolve(__dirname, "src/pages/contacto.html"),
+        aviso: path.resolve(__dirname, "src/pages/aviso-legal.html"),
+      },
+    },
+  },
+});
