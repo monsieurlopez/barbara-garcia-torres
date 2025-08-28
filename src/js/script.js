fb5c4c435bbfb15a1684a70c3f9d452d46230f15
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-08-28 20:48:48 +0200

    <fix> create file script.js for vercel insights

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+const script = document.createElement("script");
+script.src = "/_vercel/insights/script.js";
+script.defer = true;
+document.head.appendChild(script);
+
+window.va =
+  window.va ||
+  function () {
+    (window.vaq = window.vaq || []).push(arguments);
+  };
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        index: path.resolve(__dirname, "src/index.html"),
+        index: path.resolve(__dirname, "index.html"),
         lactancia: path.resolve(__dirname, "src/pages/lactancia.html"),
         soins: path.resolve(__dirname, "src/pages/soins.html"),
         contacto: path.resolve(__dirname, "src/pages/contacto.html"),
