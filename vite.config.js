6d5ef0916e21d531dbabe0c4663599fd87883703
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-08-29 02:29:19 +0200

    <fix> update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
         index: path.resolve(__dirname, "index.html"),
         lactancia: path.resolve(__dirname, "public/pages/lactancia.html"),
         soins: path.resolve(__dirname, "public/pages/soins.html"),
-        contacto: path.resolve(__dirname, "src/contacto.html"),
+        contacto: path.resolve(__dirname, "public/pages/contacto.html"),
         aviso: path.resolve(__dirname, "public/pages/aviso-legal.html"),
       },
     },
